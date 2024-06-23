83e41db76df6764489d656a98d49baed48c9945a
곽현우 <polla723@naver.com>
2024-06-23 15:16:59 +0900

    Initial Commit

---
+++ practice_App/practice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_AppApp.swift
+//  practice_App
+//
+//  Created by 곽현우 on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct practice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
