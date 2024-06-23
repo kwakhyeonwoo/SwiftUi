99aa65213c2f858ff7a390712c0f15c945745e64
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-06-23 15:59:36 +0900

    Add files via upload

---
+++ first_practice/first_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_practiceApp.swift
+//  first_practice
+//
+//  Created by 곽현우 on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct first_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
